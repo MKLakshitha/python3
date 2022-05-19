a5f65daefc0352fe6163a6d7d426317a6a640dc8
Kavindu Lakshitha <Kavindulakshitha84@gmail.com>
2022-05-19 14:46:46 +0530

    add

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Kavindu Lakshitha on 2022-05-16.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
